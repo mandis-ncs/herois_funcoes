fdb647e84a5b36e5de9a5c6779c5f2fa20063d43
unknown <amnd.castro@gmail.com>
2022-09-29 21:47:55 -0300

    vs1

---
+++ js/quizz.js
@@ -0,0 +1,20 @@
+universe=alert("Clique no seu heroi favorito!");
+
+function tocaSomBatman() {
+    document.querySelector('#somBatman').play();
+}
+
+function tocaSomCoringa() {
+    document.querySelector('#somCoringa').play();
+}
+
+function tocaSomSuperman() {
+    document.querySelector('#somSuperman').play();
+}
+
+function tocaSomGroot() {
+    document.querySelector('#somGroot').play();
+}
+
+document.querySelector('.batman');
+
